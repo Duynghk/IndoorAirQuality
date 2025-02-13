e488b58bb258a3aac565449d79f142efc0403bb3
Duynghk <133846786+Duynghk@users.noreply.github.com>
2025-02-13 12:23:38 +0700

    Change port

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def handle_mqtt_message(client, userdata, message):
 
 if __name__ == '__main__':
     # app.run(debug=True,host='0.0.0.0',port='5000')
-    serve(app, host='0.0.0.0', port=5000)
+    serve(app, host='0.0.0.0')
